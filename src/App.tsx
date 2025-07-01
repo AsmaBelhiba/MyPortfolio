22f30b05d679f4dda0cbd6fec33f1c72b1ea3d9b
Asma Benhiba <146117890+AsmaBelhiba@users.noreply.github.com>
2025-07-01 14:52:17 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       title: "CCNA: Introduction to Networks",
       issuer: "Cisco",
       date: "2025",
-      description: "Introduction to Networks covers the basics of networking, including IP addressing, Ethernet, routing, and switching.",
+      description: "Learned core networking concepts including IP addressing, Ethernet, switching, and routing, and gained hands-on experience in configuring and troubleshooting basic networks.",
       skills: ["IP connectivity", "IP services", "IPv4 And IPv6 Addressing", "Network Fundamentals"],
       verifyUrl: "https://www.credly.com/badges/69407760-f368-4478-bd1b-7ad786eb0cde/linked_in_profile" // Add your verification URL
     },
